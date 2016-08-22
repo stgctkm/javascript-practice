5d6dbf7168ca63bcbcf88b9f23b9ce5c712ae6f1
setoguchi <setoguchi.takuma@gmail.com>
2016-08-23 00:42:23 +0900

    add marker, geocode

--- map/map.js
+++ map/map.js
@@ -1,15 +1,53 @@
 (function() {
+
+  var map;
+  var init = function() {
+
     var position = new google.maps.LatLng(0, 0);
 
     var options = {
-        zoom: 2,
+        zoom: 3,
         center: position,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     var mapElement = document.getElementById("map");
 
-    var map = new google.maps.Map(mapElement, options);
+    map = new google.maps.Map(mapElement, options);
+
+    google.maps.event.addListener(map, "click", mapClick);//todo 引数でmapオブジェクトを渡せるか？
+  };
+
+  var mapClick = function(event) {
+    mark(event);
+    var geocoder = new google.maps.Geocoder();
+    cp = event.latLng;
+    var latLng = {latLng: cp};//todo global variable
+    geocoder.geocode(latLng, getResult);
+  };
+
+  var mark = function(event) {
+     var position = event.latLng;
+     var pointer = {
+        position: position,
+        map: map,
+        title: "destination"
+     };
+     var maker = new google.maps.Marker(pointer);
+  };
+
+  var getResult = function(response, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (response) {
+        map.setCenter(cp);
+        console.log(response[0].formatted_address);
+      }
+
+    } else {
+      console.log('you cannot use geocoding');
+    }
+  };
+
+  init();
 
 }());
-
